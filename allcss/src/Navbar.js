0766b84cdba3be3aa41520ea1c97bf781eb0e090
rohit <rohitshahni121@gmail.com>
2022-09-16 12:38:18 +0530

    intial commit 2

---
+++ allcss/src/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='maincontainer'>
+        <div className='items'><button className='items-btn'>b1</button></div>
+        <div className='items'><button className='items-btn'>b2</button></div>
+        <div className='items'><button className='items-btn'>b3</button></div>
+        <div className='items'><button className='items-btn'>b4</button></div>
+        <div className='items'><button className='items-btn'>b5</button></div>
+        <div className='items'><button className='items-btn'>b6</button></div>
+        <div className='items'><button className='items-btn'>b7</button></div>
+        <div className='items'><button className='items-btn'>b8</button></div>
+        <div className='items'><button className='items-btn'>b9</button></div>
+    </div>
+  )
+}
+
+export default Navbar;
+
+
+
---
+++ allcss/src/components/Flexbox.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Flexbox.css';
+
+const Flexbox = () => {
+  return (
+
+    <div className="mainbox">
+        
+
+        <div className="mainbox1">
+   <div className="flex_container1">
+    <div className="fName1 item1">1</div>
+    <div className="fName1 item2">2</div>
+    <div className="fName1 item3">3</div>
+    <div className="fName1 item4">4</div>
+   </div>
+
+   <div class="footer">power by spotmentor</div>
+</div>
+
+
+<div className="mainbox2-3">
+   <div className="flex_container2">
+    <div className="fName2">1</div>
+    <div className="fName2">2</div>
+    <div className="fName2">3</div>
+    <div className="fName2">4</div>
+   </div>
+   <div className="flex_container3">
+    <div className="fName3">1</div>
+    <div className="fName3">2</div>
+    <div className="fName3">3</div>
+    <div className="fName3">4</div>
+   </div>
+</div>
+</div>
+  )
+}
+
+export default Flexbox;
